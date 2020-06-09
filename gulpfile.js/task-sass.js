74de660e614128c954e52e4e142fd36c05b23d4b
mutantcrow <ea78200@gmail.com>
2020-06-09 19:09:52 +0300

    External node modules path added.

--- gulpfile.js/task-sass.js
+++ gulpfile.js/task-sass.js
@@ -1,12 +1,23 @@
 const {src, dest} = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-dart-sass');
 
 module.exports = () => {
   if (typeof packageJson.entry.scss !== 'undefined') {
-    callbacks.push(sass);
+    callbacks.push(compileSASS);
   }
 };
 
-const sass = (cb) => {
+const compileSASS = () => {
+  const sassArgs = {includePaths: []};
+
+  if (typeof packageJson.externalModulePath !== 'undefined') {
+    sassArgs.includePaths.push(packageJson.externalModulePath);
+  }
+
   return src(packageJson.entry.scss)
+      .pipe(sourcemaps.init())
+      .pipe(sass(sassArgs).on('error', sass.logError))
+      .pipe(sourcemaps.write())
       .pipe(dest(packageJson.output.scss));
 };
