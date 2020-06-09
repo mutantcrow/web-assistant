16497ebaacfdaee78f2c82cf53c552f33463de1b
mutantcrow <ea78200@gmail.com>
2020-06-09 21:21:00 +0300

    Util functions combined under utils.js

---
+++ utils.js
@@ -0,0 +1,16 @@
+module.exports.addLastSlash = (input) => {
+  if (input[input.length - 1] === '/') return input;
+  return input + '/';
+};
+
+module.exports.getMatchedFiles = (regex, files) => {
+  const matchedFiles = [];
+
+  files.forEach((file) => {
+    if (file.search(regex) !== -1) {
+      matchedFiles.push(file);
+    }
+  });
+
+  return matchedFiles;
+};
