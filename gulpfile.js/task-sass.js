40130142de12eb8ca639a652861710f6b5e9aa15
mutantcrow <ea78200@gmail.com>
2020-06-09 20:30:45 +0300

    Postcss and its plugins added.

--- gulpfile.js/task-sass.js
+++ gulpfile.js/task-sass.js
@@ -1,23 +1,38 @@
 const {src, dest} = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-dart-sass');
+const postcss = require('gulp-postcss');
+const postcssImport = require('postcss-import');
+const postcssCopy = require('postcss-copy');
+const postcssAutoprefixer = require('autoprefixer');
 
 module.exports = () => {
   if (typeof packageJson.entry.scss !== 'undefined') {
-    callbacks.push(compileSASS);
+    callbacks.parallel.push(taskSass);
   }
 };
 
-const compileSASS = () => {
+const taskSass = () => {
   const sassArgs = {includePaths: []};
 
   if (typeof packageJson.externalModulePath !== 'undefined') {
     sassArgs.includePaths.push(packageJson.externalModulePath);
   }
 
+  const postcssPlugins = [
+    postcssImport(),
+    postcssAutoprefixer(),
+    postcssCopy({
+      basePath: ['./'],
+      dest: packageJson.output.scss,
+      template: '[name]-[hash].[ext]',
+    }),
+  ];
+
   return src(packageJson.entry.scss)
       .pipe(sourcemaps.init())
       .pipe(sass(sassArgs).on('error', sass.logError))
+      .pipe(postcss(postcssPlugins))
       .pipe(sourcemaps.write())
       .pipe(dest(packageJson.output.scss));
 };
