7b2879024f3d1239ee88617c1ae9fc54876b673f
mutantcrow <ea78200@gmail.com>
2020-06-10 16:07:28 +0300

    execSync object destructured.

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 const {resolve} = require('path');
-const execSync = require('child_process').execSync;
+const {execSync} = require('child_process');
 
 process.env.PRODUCTION = process.argv[2] === 'production';
 process.env.CALLER_DEST = process.cwd();
